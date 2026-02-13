639cc5010022fd08bb33f6848783f090e2b115a1
JHud <219741840+JamHudson@users.noreply.github.com>
2026-02-12 21:55:36 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+app.get
+app.post
+
+res.send()
+res.sendFile(`${import.meta.dirname}/views/>>PAGENAME<<.html`)
+*/
+
+// Import express module.
+import express from 'express';
+
+const app = express();
+
+const PORT = 3001; // Specifically use Port 3001
+
+// Enable static file serving
+app.use(express.static('public'));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// Root page
+app.get('/', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/', (req, res) => {
+    // Form submitted
+});
+
+
+// Link to test locally
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
